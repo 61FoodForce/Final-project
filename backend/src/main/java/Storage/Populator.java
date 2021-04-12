d8aaae102fa3ad5f82e5f3d1ce765b9134f7c2b7
Eduardo Colon <Eddiecsworld76@gmail.com>
2021-04-12 12:14:18 -0400

    Updated POJO and added Populator

---
+++ backend/src/main/java/Storage/Populator.java
@@ -0,0 +1,16 @@
+package Storage;
+
+import POJO.Donation;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Populator implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+    }
+}
