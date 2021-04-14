54a289ffc8d5bd9dfd479552f55c005253b46ec3
Eduardo Colon <Eddiecsworld76@gmail.com>
2021-04-14 11:27:38 -0400

    Updated Controllers end point

--- backend/src/main/java/org/foodforce/mvcapp/Controllers/businessController.java
+++ backend/src/main/java/org/foodforce/mvcapp/Controllers/businessController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @Controller
+@RequestMapping("/businesses")
 public class businessController {
     private BusinessStorage businessStorage;
 
