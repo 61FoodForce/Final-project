9f277c28ffa5e8bbfa05cd34ea155f5d91cf9228
Lana Milbern <lmilbern91@gmail.com>
2021-04-14 18:47:07 -0400

    donations from populator display

--- backend/src/main/java/org/foodforce/mvcapp/Controllers/donationsController.java
+++ backend/src/main/java/org/foodforce/mvcapp/Controllers/donationsController.java
@@ -8,19 +8,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/donations")
 public class donationsController {
     private DonationStorage donationStorage;
-
     public donationsController(DonationStorage donationStorage) {
         this.donationStorage = donationStorage;
     }
 
-    @GetMapping("{id}")
-    public String displayDonations(@PathVariable long id, Model model) {
-        model.addAttribute("donations", donationStorage.retrieveDonationById(id));
-        return "";
+    // Displays all donations to donation display page.
+    @RequestMapping("/donations")
+    public String displayAllDonations(Model model) {
+        model.addAttribute("donations", donationStorage.retrieveAllDonations());
+        return "donation-display-page";
     }
+
+    //Allows user to search donations
+//    @RequestMapping("/donations")
+//    public String displayDonations(@PathVariable long id, Model model) {
+//        model.addAttribute("donations", donationStorage.retrieveDonationById(id));
+//        return "donation-display-page";
+//    }
 
 //    @PostMapping("/submitDonation")
 //    public String addDonation(@PathVariable long id, @RequestParam String name, @RequestParam Business business, @RequestParam int foodQuantity, @RequestParam int foodUnit) {
@@ -31,7 +37,7 @@ public class donationsController {
     public String addDonation(String _name, Business _business, int _foodQuantity) {
         Donation donationToAdd = new Donation(_name, _business, _foodQuantity, Donation.Unit.CAN);
         donationStorage.saveDonation(donationToAdd);
-        return "donation-display";
+        return "redirect: /donations/";
     }
 
     @DeleteMapping("{id}")
